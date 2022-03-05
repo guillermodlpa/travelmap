00bb60c6b6cebfc04a760112621a7456437a050b
Guillermo de la Puente <g.puente.allott@gmail.com>
2022-03-05 13:36:36 +0100

    Add highlighting and click events on map

--- components/Map/Map.tsx
+++ components/Map/Map.tsx
@@ -32,10 +32,69 @@ const Map: React.FC = () => {
         'country-label'
       );
 
+      map.addLayer({
+        id: 'country-fills',
+        type: 'fill',
+        source: {
+          type: 'vector',
+          url: 'mapbox://mapbox.country-boundaries-v1',
+        },
+        'source-layer': 'country_boundaries',
+        layout: {},
+        paint: {
+          'fill-color': '#fff',
+          'fill-opacity': 0, // @todo, figure out a better way
+        },
+      });
+
+      map.addLayer({
+        id: 'country-fills-hover',
+        type: 'fill',
+        source: {
+          type: 'vector',
+          url: 'mapbox://mapbox.country-boundaries-v1',
+        },
+        'source-layer': 'country_boundaries',
+        layout: {},
+        paint: {
+          'fill-color': '#627BC1',
+          'fill-opacity': 1,
+        },
+        filter: ['==', 'name', ''],
+      });
+
       // https://github.com/ecrmnn/iso-3166-1/blob/master/src/iso-3166.ts
       const highlightedCountries = [iso.whereCountry('Spain')?.alpha3, iso.whereCountry('France')?.alpha3];
 
       map.setFilter('country-boundaries', ['in', 'iso_3166_1_alpha_3', ...highlightedCountries]);
+
+      // When the user moves their mouse over the page, we look for features
+      // at the mouse position (e.point) and within the states layer (states-fill).
+      // If a feature is found, then we'll update the filter in the state-fills-hover
+      // layer to only show that state, thus making a hover effect.
+      map.on('mousemove', function (e) {
+        var features = map.queryRenderedFeatures(e.point, { layers: ['country-fills'] });
+        if (features.length) {
+          map.getCanvas().style.cursor = 'pointer';
+          map.setFilter('country-fills-hover', ['==', 'name', features[0].properties?.name]);
+        } else {
+          map.setFilter('country-fills-hover', ['==', 'name', '']);
+          map.getCanvas().style.cursor = '';
+        }
+      });
+
+      // Reset the country-fills-hover layer's filter when the mouse leaves the map
+      map.on('mouseout', function () {
+        map.getCanvas().style.cursor = 'auto';
+        map.setFilter('country-fills-hover', ['==', 'name', '']);
+      });
+
+      map.on('click', function (e) {
+        var features = map.queryRenderedFeatures(e.point, { layers: ['country-fills'] });
+        if (features.length) {
+          console.log('map click', features[0].properties?.iso_3166_1_alpha_3);
+        }
+      });
     });
 
     return () => map.remove(); // @todo: confirm this is the function
