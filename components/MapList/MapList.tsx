a1ce0f231c235d95e208e7e14fe8603a383282c0
Guillermo de la Puente <g.puente.allott@gmail.com>
2022-03-26 17:48:38 +0100

    Fix MapList responsiveness by writing custom component without using grommet List

--- components/MapList/MapList.tsx
+++ components/MapList/MapList.tsx
@@ -1,50 +1,60 @@
-import { Button, List, Box, Avatar, Text } from 'grommet';
+import { Button, List, Box, Avatar, Text, ResponsiveContext } from 'grommet';
 import NextLink from 'next/link';
+import { useContext } from 'react';
 import getTravelMapNameForUsers from '../../util/getTravelMapName';
 
 const MapList: React.FC<{
   showEditButton: boolean;
   mapList: Array<{ travelMap: TravelMap; users: User[] }>;
-}> = ({ mapList, showEditButton }) => (
-  <List
-    itemKey={(item) => item.travelMap.id}
-    data={mapList}
-    defaultItemProps={{ wrap: true }}
-    action={(item) => (
-      <Box key={`action-${item}`} direction="row" gap="small">
-        {showEditButton && (
-          <NextLink href={`/map/${item.travelMap.id}/edit`} passHref>
-            <Button label="Edit" />
-          </NextLink>
-        )}
+}> = ({ mapList, showEditButton }) => {
+  const size = useContext(ResponsiveContext);
+  console.log('size', size);
+  return (
+    <Box as="ul" pad="0" margin="0" role="list">
+      {mapList.map((item, index) => (
+        <Box
+          key={item.travelMap.id}
+          pad="small"
+          gap="small"
+          as="li"
+          wrap
+          border={{ side: index === 0 ? 'horizontal' : 'bottom', color: 'border', size: '1px' }}
+          direction={size === 'small' ? 'column' : 'row'}
+        >
+          <Box direction="row" gap="small" align="center" flex="grow">
+            <Box direction="row" flex={{ shrink: 0 }}>
+              {item.users.map((user, index) => (
+                <Avatar
+                  key={user.id}
+                  background="paper"
+                  border={{ color: 'brand', size: 'small' }}
+                  margin={{ left: `-${24 * index}px` }}
+                  style={{ zIndex: item.users.length - index }}
+                >
+                  {user.name.substring(0, 1)}
+                </Avatar>
+              ))}
+            </Box>
+            <Box flex={{ grow: 1, shrink: 1 }}>
+              <Text>{getTravelMapNameForUsers(item.users)}</Text>
+            </Box>
+          </Box>
 
-        <NextLink href={`/map/${item.travelMap.slug}`} passHref>
-          <Button label="View" />
-        </NextLink>
-      </Box>
-    )}
-  >
-    {(item: { users: User[]; travelMap: TravelMap }) => (
-      <Box direction="row" gap="small" align="center">
-        <Box direction="row">
-          {item.users.map((user, index) => (
-            <Avatar
-              key={user.id}
-              background="paper"
-              border={{ color: 'brand', size: 'small' }}
-              margin={{ left: `-${24 * index}px` }}
-              style={{ zIndex: item.users.length - index }}
-            >
-              {user.name.substring(0, 1)}
-            </Avatar>
-          ))}
+          <Box key={`action-${item}`} direction="row" gap="small" align="center" justify="end">
+            {showEditButton && (
+              <NextLink href={`/map/${item.travelMap.id}/edit`} passHref>
+                <Button label="Edit" />
+              </NextLink>
+            )}
+
+            <NextLink href={`/map/${item.travelMap.slug}`} passHref>
+              <Button label="View" />
+            </NextLink>
+          </Box>
         </Box>
-        <Box flex="grow">
-          <Text>{getTravelMapNameForUsers(item.users)}</Text>
-        </Box>
-      </Box>
-    )}
-  </List>
-);
+      ))}
+    </Box>
+  );
+};
 
 export default MapList;
