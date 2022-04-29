e89f86d5bacd21d2a534997570ca03ce040f6af2
Guillermo de la Puente <g.puente.allott@gmail.com>
2022-04-29 21:58:01 +0200

    Fix no results message not showing in recent created maps

--- components/MapList/RecentMapsList.tsx
+++ components/MapList/RecentMapsList.tsx
@@ -6,13 +6,14 @@ import NoResults from './NoResults';
 export default function RecentMapsList() {
   const { data, loading, error } = useRecentMapList();
 
-  if (!data && !loading && !error) {
+  if (data && data.length === 0) {
     return <NoResults />;
   }
 
   return (
     <>
       {error && <Text color="status-error">{error.message || 'Error'}</Text>}
+
       <MapList mapList={data || []} allowEdit={false} />
     </>
   );
