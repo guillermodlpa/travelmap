5bdb263e4452a8866e153c29a8488fc76fd52b9b
Guillermo de la Puente <g.puente.allott@gmail.com>
2022-04-29 12:20:24 +0200

    Add small margin above legend actions

--- components/Legend/LegendActions.tsx
+++ components/Legend/LegendActions.tsx
@@ -2,7 +2,14 @@ import { Box } from 'grommet';
 
 export default function LegendActions({ children }: { children: React.ReactNode }) {
   return (
-    <Box gap="xsmall" align="center" direction="row" justify="end" flex={{ shrink: 0 }}>
+    <Box
+      gap="xsmall"
+      align="center"
+      direction="row"
+      justify="end"
+      flex={{ shrink: 0 }}
+      margin={{ top: 'small' }}
+    >
       {children}
     </Box>
   );
