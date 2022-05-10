c62afe1eb23e341c4a4e12b4357f43513263819b
Guillermo de la Puente <g.puente.allott@gmail.com>
2022-05-10 11:34:13 +0200

    Add share meta tags and og:image

--- components/HeadWithDefaults/HeadWithDefaults.tsx
+++ components/HeadWithDefaults/HeadWithDefaults.tsx
@@ -1,17 +1,29 @@
 import Head from 'next/head';
 import { useTheme } from 'styled-components';
 
+const META_DESCRIPTION =
+  'A map of the countries you have visited. Sign up, create your map, and combine it with the maps of your friends.';
+
 export default function HeadWithDefaults({ title }: { title: string }) {
   const theme = useTheme();
   return (
     <Head>
       {title && <title>{title}</title>}
+      <meta name="description" content={META_DESCRIPTION} />
+      <meta name="author" content="Guillermo de la Puente" />
       <meta
         name="theme-color"
         content={
           theme.dark ? theme.global.colors.parchment.dark : theme.global.colors.parchment.light
         }
       ></meta>
+      <meta
+        property="og:image"
+        content="https://https://travelmap.guillermodlpa.com/landing-page-screenshot.png"
+      />
+      <meta property="og:title" content={title} />
+      <meta property="og:locale" content="en_US" />
+      <meta property="og:type" content="website" />
     </Head>
   );
 }
